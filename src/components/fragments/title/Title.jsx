49832ae713e3f5b34e398470e82fa17a00cd9989
vyzaa <rafvy.muhammad@gmail.com>
2024-11-08 22:21:46 +0700

    perbaikan title

--- src/components/fragments/title/Title.jsx
+++ src/components/fragments/title/Title.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-const CategoryTitle = () => {
-  return (
-    <div className="flex justify-between items-center mb-6">
-      <h2 className="text-2xl font-bold">Browse By Category</h2>
-      <a href="#" className="text-red-500 hover:underline">
-        View All
-      </a>
-    </div>
-  );
-};
-
-export default CategoryTitle;
+export default function Title() {
+    return (
+        <div className="flex justify-between items-center mb-6">
+            <h2 className="text-2xl font-bold">Browse By Category</h2>
+            <a href="#" className="text-red-500 hover:underline">
+                View All
+            </a>
+        </div>
+    );
+}
